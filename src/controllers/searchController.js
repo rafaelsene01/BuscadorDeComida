8d3cea77da6057af339612e66fa952b480432497
Rafael Sene <rafaelssene@gmail.com>
2019-04-30 03:32:22 -0300

    teste 1

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -4,8 +4,8 @@ const repository = require('../repositories/searchFood.js');
 
 exports.get = async (req, res, next) => {
     try {
-        var data = await repository.get(req.query.food);
-        res.status(200).send(data);
+        //var data = await repository.get(req.query.food);
+        res.status(200).send(req.query.food);
     } catch (e) {
         res.status(500).send({
             message: 'Falha ao processar sua requisição'
