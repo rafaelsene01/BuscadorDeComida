39d6f464742471ccbf66b2ad1af813a9741c98e8
Rafael Sene <rafaelssene@gmail.com>
2019-04-30 03:39:52 -0300

    teste 3

--- src/repositories/searchFood.js
+++ src/repositories/searchFood.js
@@ -5,10 +5,11 @@ const seumercadoonline = require('../sites/seumercadoonline');
 
 exports.get = async (nome_limento) => {
 
+    return nome_limento;
     let lista_alimentos = [];
 
     await Promise.all([domuscatore.alimento(nome_limento), seumercadoonline.alimento(nome_limento)]).then(function (resposta) {
-        await resposta.forEach(element => {
+        resposta.forEach(element => {
             element.forEach(alimento => {
                 lista_alimentos.push(alimento);
             })
