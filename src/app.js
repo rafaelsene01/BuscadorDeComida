f348096d02f129b7de9d39689e8e7b5348e8644e
Rafael Sene <rafaelssene@gmail.com>
2019-05-01 02:30:17 -0300

    modificando cors 3

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
     res.header('Content-type: application/json');
     res.header('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
