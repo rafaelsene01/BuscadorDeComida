a9f5d0031f42b2f790436b492a13c00b928a091c
Rafael Sene <rafaelssene@gmail.com>
2019-05-01 03:32:02 -0300

    modificando cors 9

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const cors = require('cors');
 
 //Rotas
 
@@ -13,14 +12,12 @@ app.use('/', index);
 app.use('/search', search);
 
 // Habilita o CORS/*
-app.use(cors());
-/*
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
-*/
 
 module.exports = app; 
