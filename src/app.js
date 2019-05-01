042748e36d2c09edbe6502d7b221297619dfd074
Rafael Sene <rafaelssene@gmail.com>
2019-05-01 02:50:19 -0300

    modificando cors 4

--- src/app.js
+++ src/app.js
@@ -13,11 +13,9 @@ app.use('/search', search);
 
 // Habilita o CORS/*
 app.use(function (req, res, next) {
-    res.header('Content-type: application/json');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 
