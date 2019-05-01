6a12292369929529637806bddabb456265b988be
Rafael Sene <rafaelssene@gmail.com>
2019-05-01 02:18:13 -0300

    modificando cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use('/search', search);
 // Habilita o CORS/*
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
